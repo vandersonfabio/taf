d30c1173eb1c88b313209087a05864a893bfd334
vandersonfabio <vanderson.fabio@gmail.com>
2021-11-11 16:52:38 -0300

    Adição de templates e views

--- taf/urls.py
+++ taf/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from taf import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('resultado/', views.resultado, name='resultado'),
 ]
